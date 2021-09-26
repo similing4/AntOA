58d4f790405e799a32d8b515f769ffe1e6f868d6
李业宏扬 <845206213@qq.com>
2021-09-26 18:15:30 +0800

    Update HeaderAvatar.vue

--- frontend/src/layouts/header/HeaderAvatar.vue
+++ frontend/src/layouts/header/HeaderAvatar.vue
@@ -1,7 +1,7 @@
 <template>
 	<a-dropdown>
 		<div class="header-avatar" style="cursor: pointer">
-			<a-avatar class="avatar" size="small" shape="circle" :src="user.avatar" />
+			<a-avatar class="avatar" size="small" shape="circle" />
 			<span class="name">后台用户</span>
 		</div>
 		<a-menu :class="['avatar-menu']" slot="overlay">

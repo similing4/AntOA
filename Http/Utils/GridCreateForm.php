a127c66604b1932ed1cac193de912dd4eb251cfe
李业宏扬 <845206213@qq.com>
2021-11-01 16:23:01 +0800

    同步

--- Http/Utils/GridCreateForm.php
+++ Http/Utils/GridCreateForm.php
@@ -98,6 +98,7 @@ class GridCreateForm implements JsonSerializable {
      * @param String $colTip 在列表页该列的的表头名称
      * @param array $extra 该列类型的对应额外数据
      * @return GridCreateForm 返回this以便链式调用
+     * @deprecated
      */
     public function column($columnType, $col, $colTip, $extra = []) {
         $this->columns[] = [
@@ -105,6 +106,285 @@ class GridCreateForm implements JsonSerializable {
             "col"   => $col,
             "tip"   => $colTip,
             "extra" => $extra
+        ];
+        return $this;
+    }
+
+
+    /**
+     * 指定一个文本输入框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnText($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_TEXT,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个Textarea
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnTextarea($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_TEXTAREA,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个密码输入框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnPassword($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_PASSWORD,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个下拉单选框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @param array $keyValMap 单选的键值对数组，键为数据库的字段值，值为选择时展示的值
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnSelect($col, $colTip, array $keyValMap) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_SELECT,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => $keyValMap
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个Radio单选框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @param array $keyValMap 单选的键值对数组，键为数据库的字段值，值为选择时展示的值
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnRadio($col, $colTip, array $keyValMap) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_RADIO,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => $keyValMap
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个多选框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @param array $keyValMap 多选的键值对数组，键为数据库的字段值，值为选择时展示的值
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnCheckbox($col, $colTip, array $keyValMap) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_CHECKBOX,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => $keyValMap
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个树形多选框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @param array $keyValMap 详见https://www.antdv.com/components/tree-select-cn的tree-data属性
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnTreeCheckbox($col, $colTip, array $keyValMap) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_TREE_CHECKBOX,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => $keyValMap
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个时间选择输入框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnTimestamp($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_TIMESTAMP,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个时富文本输入框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnRichText($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_RICHTEXT,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个图片选择框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnPicture($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_PICTURE,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个文件选择框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnFile($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_FILE,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个图片多选框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnPictures($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_PICTURES,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个文件多选框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnFiles($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_FILES,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个级联选择框
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @param array $keyValMap 详见https://www.antdv.com/components/cascader-cn
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnChoose($col, $colTip, array $keyValMap) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_CHOOSE,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => $keyValMap
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个展示框，不会被提交
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnDisplay($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_DISPLAY,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个隐藏的行，会被提交，可以用来接其它页面传来的参数
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnHidden($col, $colTip) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_HIDDEN,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => []
+        ];
+        return $this;
+    }
+
+    /**
+     * 指定一个子表选择行，子表的第一个设置的列会被作为最终选择的值
+     * @param String $col 数据库列名
+     * @param String $colTip 在列表页该列的的表头名称
+     * @param GridList $gridList 用于选择的GridList实例
+     * @return GridCreateForm 返回this以便链式调用
+     */
+    public function columnChildrenChoose($col, $colTip, GridList $gridList) {
+        $this->columns[] = [
+            "type"  => self::COLUMN_CHILDREN_CHOOSE,
+            "col"   => $col,
+            "tip"   => $colTip,
+            "extra" => $gridList
         ];
         return $this;
     }

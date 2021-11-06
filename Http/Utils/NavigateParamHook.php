56e488b3ef69cfe34d23d5ac6fc61dc8803292ba
李业宏扬 <845206213@qq.com>
2021-11-06 10:19:48 +0800

    Create NavigateParamHook.php

---
+++ Http/Utils/NavigateParamHook.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * FileName:NavigateParamHook.php
+ * Author:Shengxinyu
+ * Email:845206213@qq.com
+ * Date:2021/11/6
+ * Time:10:18
+ */
+declare(strict_types=1);
+
+namespace Modules\AntOA\Http\Utils;
+
+
+interface NavigateParamHook {
+    /**
+     * 每行数据在跳转时进行的url拼接回调方法
+     * @param array $row 行数据
+     * @return string url参数字符串，注意不带问号
+     */
+    public function hook($row);
+}

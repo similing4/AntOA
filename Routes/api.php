5747309f8b73f80f1796878b95f374f30a3468bf
李业宏扬 <845206213@qq.com>
2022-03-18 13:20:20 +0800

    同步

--- Routes/api.php
+++ Routes/api.php
@@ -21,5 +21,5 @@ Route::prefix('antoa')->group(function() {
     Route::post('/auth/login', 'AuthController@login');
     Route::post('/auth/auth', 'AuthController@auth');
     Route::get('/auth/config', 'AuthController@api_config');
-    Route::post("/antoa/user/change_password","AntOAUserController@changePassword");
+    Route::post("/user/change_password","AntOAUserController@changePassword");
 });
--- Routes/web.php
+++ Routes/web.php
@@ -10,3 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('antoa')->group(function() {
+    Route::redirect('/webpack/index', '/antoa/webpack/index.html');
+});

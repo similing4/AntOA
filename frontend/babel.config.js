17b5faee4d0484db8f1b62feaed91ac5fed56af8
李业宏扬 <845206213@qq.com>
2022-04-24 13:51:23 +0800

    Update babel.config.js

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,7 +2,7 @@ const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
 
 const plugins = []
 if (IS_PROD) {
-  plugins.push('transform-remove-console')
+  //plugins.push('transform-remove-console')
 }
 
 module.exports = {

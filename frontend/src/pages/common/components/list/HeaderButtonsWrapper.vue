1bdf7469cfae733edb1ac0a606c79ad21e1d62a8
圣心语 <845206213@qq.com>
2022-06-09 15:36:56 +0800

    Update HeaderButtonsWrapper.vue

--- frontend/src/pages/common/components/list/HeaderButtonsWrapper.vue
+++ frontend/src/pages/common/components/list/HeaderButtonsWrapper.vue
@@ -157,9 +157,10 @@ export default {
 				}).call();
 				if (!res.status)
 					this.$message.error(res.msg);
-				else
+				else{
 					this.$message.success(res.data);
-				this.isShowCreateModal[index] = false;
+					this.isShowCreateModal[index] = false;
+				}
 				this.loadPage();
 				this.$forceUpdate();
 			});

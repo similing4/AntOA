1b94726c33edc002a079df5ee01e27e9746992de
圣心语 <845206213@qq.com>
2023-07-07 10:12:56 +0800

    Revert "Update .env"
    This reverts commit ecd78530707ffebb81a5bb008bd4f291a4889ce0.

--- frontend/src/pages/common/components/edit/column_column/EditColumnFileLocal.vue
+++ frontend/src/pages/common/components/edit/column_column/EditColumnFileLocal.vue
@@ -1,6 +1,5 @@
 <template>
 	<a-form-item :label="column.tip" :label-col="{span: 7}" :wrapper-col="{span: 10}">
-		{{value.split("/")[value.split("/").length - 1]}}
 		<a-button type="primary" @click="openurl(value)" v-if="value != ''">下载</a-button>
 		<a-button type="danger" @click="onChange('')" v-if="value != ''">删除</a-button>
 		<upload-button @uploadfinished="onChange($event[0].response)" accept="*/*" :multiple="false" type="edit" :col="column.col" :path="gridApiObject.api_upload"></upload-button>
